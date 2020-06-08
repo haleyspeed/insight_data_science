652b680204ce2295cbd00167ac9e169d137d8113
Haley Speed <haley.e.speed@gmail.com>
2020-06-08 08:09:15 -0700

    fixing git error

--- subscript/subscript/file_concatenator.py
+++ subscript/subscript/file_concatenator.py
@@ -4,9 +4,18 @@ import csv
 import config
 import numpy as np
 
+<<<<<<< HEAD
+dir_in = os.path.join(config.clean_dir, 'processed_player_stats')
+dir_out = config.clean_dir
+file_in = os.path.join(dir_in, '*{}')
+file_out = os.path.join(dir_out,'processed_player_concat_test.csv')
+df = cf.csv_concatenator (dir_in)
+df.to_csv(file_out)
+=======
 dir_in = os.path.join(config.clean_dir, 'time_tallied')
 dir_out = config.clean_dir
 file_in = os.path.join(dir_in, '*{}')
 file_out = os.path.join(dir_out,'timed_player_stats_subset.csv')
 df = cf.csv_concatenator (dir_in)
 df.to_csv(file_out)
+>>>>>>> c01bf7205576901f30adad57cb3c8e7686e44fb0

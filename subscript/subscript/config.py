f4fc8f5544e81f580fe8458bb161fc6c9593708f
Haley Speed <haley.e.speed@gmail.com>
2020-06-23 14:21:27 -0700

    SubScript v1.0 Final

--- subscript/subscript/config.py
+++ subscript/subscript/config.py
@@ -8,7 +8,3 @@ raw_dir = os.path.join(home_dir,'data','raw')
 processed_dir = os.path.join(home_dir, 'data', 'processed')
 clean_dir = os.path.join(home_dir,'data', 'cleaned')
 
-
-
-#customer_db_url = 'sql:///customer/db/url'
-#purchases_db_url = 'sql:///purchases/db/url'
